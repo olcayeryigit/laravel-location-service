f60533ce91c7e2e95a05535021ecab49eadbed9d
olcay <olcayeryigit@hotmail.com>
2025-03-18 13:25:40 +0300

    v

--- laravel-location-service-frontend/src/components/LocationList/LocationList.jsx
+++ laravel-location-service-frontend/src/components/LocationList/LocationList.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
-import { FaMapMarkerAlt } from "react-icons/fa";
+import { useState, useMemo } from "react";
+import { FaLocationDot } from "react-icons/fa6";
 import LocationItem from "./LocationItem";
 import EditForm from "./EditForm";
-import { CiLocationOn } from "react-icons/ci";
-import { IoLocation } from "react-icons/io5";
-import { FaLocationDot, FaMapLocation } from "react-icons/fa6";
 
 const LocationList = ({ locations, onDelete, onEdit }) => {
   const [editLocation, setEditLocation] = useState(null);
@@ -24,29 +21,32 @@ const LocationList = ({ locations, onDelete, onEdit }) => {
     setEditLocation(null);
   };
 
+  // Locations listesini optimize etmek için useMemo kullanımı
+  const memoizedLocations = useMemo(() => {
+    return locations.map((location) => (
+      <div key={location.id} className="transition-all">
+        <LocationItem location={location} onEdit={handleEditClick} onDelete={onDelete} />
+        {editLocation?.id === location.id && (
+          <EditForm
+            editLocation={editLocation}
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+            onCancel={() => setEditLocation(null)}
+          />
+        )}
+      </div>
+    ));
+  }, [locations, editLocation]); // Sadece locations veya editLocation değiştiğinde yeniden hesaplanır.
+
   return (
     <div className="p-4 bg-gray-900 shadow-lg rounded-2xl w-full max-w-md mx-auto border border-gray-700">
       {/* Başlık */}
       <h2 className="text-lg font-semibold text-white flex items-center justify-center gap-2 mb-4">
-      <FaLocationDot className="text-white text-xl" /> Location List
+        <FaLocationDot className="text-white text-xl" /> Location List
       </h2>
 
       {/* Konum Listesi */}
-      <ul className="space-y-3  ">
-        {locations.map((location) => (
-          <div key={location.id} className="transition-all">
-            <LocationItem location={location} onEdit={handleEditClick} onDelete={onDelete} />
-            {editLocation?.id === location.id && (
-              <EditForm
-                editLocation={editLocation}
-                onChange={handleChange}
-                onSubmit={handleSubmit}
-                onCancel={() => setEditLocation(null)}
-              />
-            )}
-          </div>
-        ))}
-      </ul>
+      <ul className="space-y-3">{memoizedLocations}</ul>
     </div>
   );
 };
